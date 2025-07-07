1c97f48f03c294e708b77f5ed670f040d63bc262
programmertarini <tariniprasad556@gmail.com>
2025-07-07 15:16:18 +0530

    Zentry clone

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className=" text-6xl text-violet-300 text-center font-circular-web">
+      Wellcome to Zentry
+    </main>
+  );
+};
+
+export default App;
